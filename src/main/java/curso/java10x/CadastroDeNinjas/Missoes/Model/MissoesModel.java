355c2aedd09c1159cf5c985ca36d5a13f6f1a671
higorvaluar <higorvb2001@gmail.com>
2025-03-27 14:05:01 -0300

    Trabalhando com Lombok

--- src/main/java/curso/java10x/CadastroDeNinjas/Missoes/Model/MissoesModel.java
+++ src/main/java/curso/java10x/CadastroDeNinjas/Missoes/Model/MissoesModel.java
@@ -2,11 +2,17 @@ package curso.java10x.CadastroDeNinjas.Missoes.Model;
 
 import curso.java10x.CadastroDeNinjas.Ninjas.Model.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table (name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
 
     @Id
@@ -18,31 +24,4 @@ public class MissoesModel {
     // @OneToMany - Uma missão pode ter vários ninjas
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
-
-    public MissoesModel() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
 }
--- src/main/java/curso/java10x/CadastroDeNinjas/Ninjas/Model/NinjaModel.java
+++ src/main/java/curso/java10x/CadastroDeNinjas/Ninjas/Model/NinjaModel.java
@@ -2,10 +2,16 @@ package curso.java10x.CadastroDeNinjas.Ninjas.Model;
 
 import curso.java10x.CadastroDeNinjas.Missoes.Model.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 // Entity transforma uma classe em uma entidade do BD
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor // Construtor vazio
+@AllArgsConstructor // Construtor cheio
+@Data // Getters e Setters
 public class NinjaModel {
 
     @Id
@@ -19,37 +25,4 @@ public class NinjaModel {
     @ManyToOne
     @JoinColumn(name = "missoes_id") // Foreing Key
     private MissoesModel missoes;
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }

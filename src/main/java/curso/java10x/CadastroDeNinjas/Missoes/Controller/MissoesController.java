711320271503901fd655978c4108825a9b176134
higorvaluar <higorvb2001@gmail.com>
2025-03-31 20:00:37 -0300

    feat: Cria as rotas do missoescontroller com mock

--- src/main/java/curso/java10x/CadastroDeNinjas/Missoes/Controller/MissoesController.java
+++ src/main/java/curso/java10x/CadastroDeNinjas/Missoes/Controller/MissoesController.java
@@ -1,4 +1,32 @@
 package curso.java10x.CadastroDeNinjas.Missoes.Controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
 public class MissoesController {
+
+    // GET - Mandar uma requisição para MOSTRAR as missões
+    @GetMapping("/listar")
+    public String listarMissao() {
+        return "Missões listadas!";
+    }
+
+    // POST - Mandar uma requisição para CRIAR as missões
+    @PostMapping("/criar")
+    public String criarMissao() {
+        return "Missão criada!";
+    }
+
+    // PUT - Mandar uma requisição para ALTERAR as missões
+    @PutMapping("/alterar")
+    public String alterarMissao() {
+        return "Missão alterada!";
+    }
+
+    // DELETE - Mandar uma requisição para DELETAR as missões
+    @DeleteMapping("/deletar")
+    public String deletarMissao() {
+        return "Missão deletada!";
+    }
 }
